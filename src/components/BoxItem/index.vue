688e0779db534a86d2098a4b4a2d0e40d7937369
unknown <uittle@163.com>
2019-01-09 16:18:00 +0800

    根据客户的需求修改的

--- src/components/BoxItem/index.vue
+++ src/components/BoxItem/index.vue
@@ -3,46 +3,46 @@
         <div class="boxItem" v-if="type == 1">
             <div class="borbbx">
                 <div class="vCenter pt10">
-                    <p class="f32 cor3 fb mr15">重庆市22号线地铁一期工程</p>
-                    <p class="box2 yellowbg">施工</p>
                     <p class="box2 redbg">急</p>
+                    <p class="box2 yellowbg">施工</p>
+                    <p class="f32 cor3 fb mr15">重庆市22号线地铁一期工程</p>
                 </div>
-                <div class="vCenter inlineitem mb20 mt10">
-                    <p class="f20 cor6">二级建造师(4)</p>
-                    <p class="f20 cor6">施工员(17)</p>
-                    <p class="f20 cor6">安全员(17)</p>
-                    <p class="f20 cor6">项目经理(17)</p>
-                    <p class="f20 cor6">材料员(17)</p>
+                <div class="henglineleft inlineitem mb20 mt10">
+                    <p class="f24 cor6">二级建造师(4)</p>
+                    <p class="f24 cor6">施工员(17)</p>
+                    <p class="f24 cor6">安全员(17)</p>
+                    <p class="f24 cor6">项目经理(17)</p>
+                    <p class="f24 cor6">材料员(17)</p>
                 </div>
             </div>
             <div class="heng dibu">
                 <div>
-                    <div class="vCenter mb15"><div class="boxkk greenk">建筑工程</div><div class="boxkk redk">江北区</div></div>
+                    <div class="vCenter"><div class="boxkk redk">建筑工程</div><div class="boxkk redk">江北区</div></div>
+                </div>
+                <div class="vCenter">
                     <div class="vCenter">
                         <div class="deage"><img class="wp100" src="@a/Images/png/pic1.png" alt=""></div>
-                        <div class="deage"><img class="wp100" src="@a/Images/png/pic1.png" alt=""></div>
+                        <div class="deage"><img class="wp100" src="@a/Images/png/icon5.png" alt=""></div>
                         <p class="f20 cor6 pl10">张益达</p>
                     </div>
-                </div>
-                <div class="chuizhiR">
-                    <p class="themefont f28 lh10">50天</p>
-                    <p class="cor9 f20 mt20">刚刚</p>
+                    <!-- <p class="themefont f28 lh10">50天</p> -->
+                    <p class="cor9 f20 ml20">刚刚</p>
                 </div>
             </div>
         </div>
         <div class="boxItem" v-if="type == 2"  style="padding-top:10px">
             <div class="borbbx">
                 <div class="vCenter pt10">
-                    <p class="f32 cor3 fb mr15">重庆市22号线地铁一期工程</p>
-                    <p class="box2 yellowbg">施工</p>
                     <p class="box2 redbg">急</p>
+                    <p class="box2 yellowbg">施工</p>
+                    <p class="f32 cor3 fb mr15">重庆市22号线地铁一期工程</p>
                 </div>
-                <div class="vCenter inlineitem mb20 mt10">
-                    <p class="f20 cor6">二级建造师(4)</p>
-                    <p class="f20 cor6">施工员(17)</p>
-                    <p class="f20 cor6">安全员(17)</p>
-                    <p class="f20 cor6">项目经理(17)</p>
-                    <p class="f20 cor6">材料员(17)</p>
+                <div class="henglineleft inlineitem mt10 mb20">
+                    <p class="f24 cor6">二级建造师(4)</p>
+                    <p class="f24 cor6">施工员(17)</p>
+                    <p class="f24 cor6">安全员(17)</p>
+                    <p class="f24 cor6">项目经理(17)</p>
+                    <p class="f24 cor6">材料员(17)</p>
                 </div>
             </div>
             <div class="heng dibu">
@@ -55,24 +55,28 @@
             <div class="borbbx">
                 <div class="heng vCenter pt10">
                     <div class="vCenter">
-                        <p class="f32 cor3 fb mr15">项目经理</p>
-                        <p class="box2 yellowbg">施工</p>
                         <p class="box2 redbg">急</p>
+                        <p class="box2 yellowbg">施工</p>
+                        <p class="f32 cor3 fb mr15">项目经理</p>
                     </div>
                     <span class="f24" style="color: #fe6e63;">面议</span>
                 </div>
-                <div class="vCenter mb20 mt20">
-                    <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
-                    <span class="f20 cor9 ml30">意向明细：</span><div class="boxkk greenk">施工-建筑工程</div>
+                <div class="mb20 mt20">
+                    <div class="vCenter mb10">
+                        <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
+                    </div>
+                    <div class="vCenter">
+                        <span class="f20 cor9">意向明细：</span><div class="boxkk redk">施工-建筑工程</div>
+                    </div>
                 </div>
             </div>
-            <div class="heng dibu">
-                <div class="vCenter inlineitem mb20 ">
-                    <p class="f20 cor6">二级建造师(4)</p>
-                    <p class="f20 cor6">施工员(17)</p>
-                    <p class="f20 cor6">安全员(17)</p>
-                    <p class="f20 cor6">项目经理(17)</p>
-                    <p class="f20 cor6">材料员(17)</p>
+            <div class="heng pb20 mt20">
+                <div class="henglineleft inlineitem ">
+                    <p class="f24 cor6">二级建造师(4)</p>
+                    <p class="f24 cor6">施工员(17)</p>
+                    <p class="f24 cor6">安全员(17)</p>
+                    <p class="f24 cor6">项目经理(17)</p>
+                    <p class="f24 cor6">材料员(17)</p>
                 </div>
             </div>
         </div>
@@ -80,35 +84,40 @@
             <div class="borbbx">
                 <div class="heng vCenter pt10">
                     <div class="vCenter">
-                        <p class="f32 cor3 fb mr15">项目经理</p>
                         <p class="box2 zibg">工人</p>
+                        <p class="f32 cor3 fb mr15">项目经理</p>
                     </div>
-                    <span class="f24" style="color: #fe6e63;">6000-800</span>
+                    <span class="f24 fb" style="color: #fe6e63;">6000-800</span>
                 </div>
-                <div class="vCenter mb20 mt20">
-                    <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
-                    <span class="f20 cor9 ml30">意向明细：</span><div class="boxkk greenk">施工-建筑工程</div>
+                <div class="mb20 mt20">
+                    <div class="vCenter mb10">
+                        <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
+                    </div>
+                    <div class="vCenter">
+                        <span class="f20 cor9">意向明细：</span><div class="boxkk redk">施工-建筑工程</div>
+                    </div>
                 </div>
             </div>
-            <div class="heng pt15 pb15">
-                <div class="vCenter inlineitem ">
-                    <p class="f20 cor6">二级建造师(4)</p>
-                    <p class="f20 cor6">施工员(17)</p>
-                    <p class="f20 cor6">安全员(17)</p>
-                    <p class="f20 cor6">项目经理(17)</p>
-                    <p class="f20 cor6">材料员(17)</p>
+            <div class="heng pb10 mt20">
+                <div class="henglineleft inlineitem ">
+                    <p class="f24 cor6">二级建造师(4)</p>
+                    <p class="f24 cor6">施工员(17)</p>
+                    <p class="f24 cor6">安全员(17)</p>
+                    <p class="f24 cor6">项目经理(17)</p>
+                    <p class="f24 cor6">材料员(17)</p>
                 </div>
             </div>
             <div class="heng pb30">
                 <div>
+                </div>
+                <div class="vCenter">
                     <div class="vCenter">
                         <div class="deage"><img class="wp100" src="@a/Images/png/pic1.png" alt=""></div>
-                        <div class="deage"><img class="wp100" src="@a/Images/png/pic1.png" alt=""></div>
+                        <div class="deage"><img class="wp100" src="@a/Images/png/icon5.png" alt=""></div>
                         <p class="f20 cor6 pl10">张益达</p>
                     </div>
-                </div>
-                <div class="chuizhiR">
-                    <p class="cor9 f20">刚刚</p>
+                    <!-- <p class="themefont f28 lh10">50天</p> -->
+                    <p class="cor9 f20 ml20">刚刚</p>
                 </div>
             </div>
         </div>
@@ -116,23 +125,27 @@
             <div class="borbbx">
                 <div class="heng vCenter pt10">
                     <div class="vCenter">
-                        <p class="f32 cor3 fb mr15">项目经理</p>
-                        <p class="box2 yellowbg">施工</p>
                         <p class="box2 redbg">急</p>
+                        <p class="box2 yellowbg">施工</p>
+                        <p class="f32 cor3 fb mr15">项目经理</p>
                     </div>
                     <span class="f24" style="color: #fe6e63;">面议</span>
                 </div>
-                <div class="vCenter mb20 mt20">
-                    <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
-                    <span class="f20 cor9 ml30">意向明细：</span><div class="boxkk greenk">施工-建筑工程</div>
+                <div class="mb20 mt20">
+                    <div class="vCenter mb10">
+                        <span class="f20 cor9">意向地区：</span><div class="boxkk redk ">江北区</div>
+                    </div>
+                    <div class="vCenter">
+                        <span class="f20 cor9">意向明细：</span><div class="boxkk redk">施工-建筑工程</div>
+                    </div>
                 </div>
                 <div class="heng pb10">
-                    <div class="vCenter inlineitem ">
-                        <p class="f20 cor6">二级建造师(4)</p>
-                        <p class="f20 cor6">施工员(17)</p>
-                        <p class="f20 cor6">安全员(17)</p>
-                        <p class="f20 cor6">项目经理(17)</p>
-                        <p class="f20 cor6">材料员(17)</p>
+                    <div class="henglineleft inlineitem ">
+                        <p class="f24 cor6">二级建造师(4)</p>
+                        <p class="f24 cor6">施工员(17)</p>
+                        <p class="f24 cor6">安全员(17)</p>
+                        <p class="f24 cor6">项目经理(17)</p>
+                        <p class="f24 cor6">材料员(17)</p>
                     </div>
                 </div>
             </div>
@@ -145,23 +158,23 @@
             <div class="borbbx">
                 <div class="heng vCenter pt10">
                     <div class="vCenter">
-                        <p class="f32 cor3 fb mr15">项目经理</p>
-                        <p class="box2 yellowbg">施工</p>
                         <p class="box2 redbg">急</p>
+                        <p class="box2 yellowbg">施工</p>
+                        <p class="f32 cor3 fb mr15">项目经理</p>
                     </div>
                     <!-- <span class="f24" style="color: #fe6e63;">面议</span> -->
                 </div>
                 <div class="heng mt20">
-                    <div class="vCenter inlineitem ">
-                        <p class="f20 cor6">二级建造师(4)</p>
-                        <p class="f20 cor6">施工员(17)</p>
-                        <p class="f20 cor6">安全员(17)</p>
-                        <p class="f20 cor6">项目经理(17)</p>
-                        <p class="f20 cor6">材料员(17)</p>
+                    <div class="henglineleft inlineitem ">
+                        <p class="f24 cor6">二级建造师(4)</p>
+                        <p class="f24 cor6">施工员(17)</p>
+                        <p class="f24 cor6">安全员(17)</p>
+                        <p class="f24 cor6">项目经理(17)</p>
+                        <p class="f24 cor6">材料员(17)</p>
                     </div>
                 </div>
                 <div class="vCenter mb20 mt20">
-                    <div class="boxkk greenk">施工-建筑工程</div>
+                    <div class="boxkk redk">施工-建筑工程</div>
                     <div class="boxkk redk ">江北区</div>
                 </div>
             </div>
@@ -188,8 +201,8 @@ export default {
 .boxItem{
     .w(90%);.bag1;box-shadow: 0 3px 6px rgba(0,0,0,0.1);padding: 0 15px;.m0a;.borR(8px);.oh;
     .inlineitem{
-        p{.mr(15);.pr;}
-        p:after{.pos;right:-7.5px;top:2.5px;background:#d3d3d3;content:'';width:1px;height: 10px;}
+        p{.mr(20);.pr;}
+        p:after{.pos;right:-10px;top:2.5px;background:#d3d3d3;content:'';width:1px;height: 10px;}
         p:last-child:after{.dn;}
     }
 }
--- src/pages/Mine/MyManage/Project/index.vue
+++ src/pages/Mine/MyManage/Project/index.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
         <div class="saixuan saixuantab">
-            <div class="actived">已发布项目</div>
-            <div>已发布服务</div>
-            <div>已参与项目</div>
-            <div>已参与服务</div>
+            <div class="actived" @click="qiehuan(1)">我的项目</div>
+            <div @click="qiehuan(2)">我的服务</div>
+            <div @click="qiehuan(3)">参与项目</div>
+            <div @click="qiehuan(4)">参与服务</div>
         </div>
-        <div class="" v-if=" tab==1"> <!-- 已发布项目 -->
+        <div class="" v-if=" tab==1"> <!-- 我的项目 -->
             <router-link to="mineManageDetail">
                 <BoxItem v-for="(item,key) in 6" :key="key" :type="6" style="margin-bottom:12px;"/>
             </router-link>
         </div>
-        <div class="" v-if=" tab==2"> <!-- 已发布服务 -->
+        <div class="" v-if=" tab==2"> <!-- 我的服务 -->
             <router-link to="mineManageDetail">
                 <BoxItem v-for="(item,key) in 6" :key="key" :type="5" style="margin-bottom:12px;"/>
             </router-link>
         </div>
-        <div class="" v-if=" tab==3"> <!-- 已参与项目 -->
+        <div class="" v-if=" tab==3"> <!-- 参与项目 -->
             <router-link to="mineManageDetail">
                 <BoxItem v-for="(item,key) in 6" :key="key" :type="1" style="margin-bottom:12px;"/>
             </router-link>
         </div>
-        <div class="" v-if=" tab==4"><!-- 已参与服务 -->
+        <div class="" v-if=" tab==4"><!-- 参与服务4 -->
             <router-link to="mineManageDetail">
                 <BoxItem v-for="(item,key) in 6" :key="key" :type="4" style="margin-bottom:12px;"/>
             </router-link>
@@ -36,6 +36,11 @@ export default {
     data() {
         return {
             tab: 1
+        }
+    },
+    methods: {
+        qiehuan(index){
+            this.tab = index;
         }
     }
 }
